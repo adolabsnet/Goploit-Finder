6fab3e00ab2863c61e02ef1b0aeb6ec4ad96d8c8
BlackZacky <37225059+BlackZacky@users.noreply.github.com>
2020-10-20 21:04:29 -0300

    Update 1.1
    - New Lib: Flag
    - New Params: -name, -range

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"net/http"
-	"os"
 )
 
 type ExploitApiJson []struct {
@@ -14,18 +14,13 @@ type ExploitApiJson []struct {
 }
 
 func main() {
-	var exploitName string
+	argparserPage := flag.Int("range", 20, "goploit -range [Number]")
+	argparserName := flag.String("name", "wordpress", "goploit -name [Framework Name]")
+	flag.Parse()
 
 	Banner()
 
-	if len(os.Args) <= 1 {
-		fmt.Print("Enter the name of the Exploit or Framework (Web): ")
-		fmt.Scanf("%s", &exploitName)
-	} else {
-		exploitName = os.Args[1]
-	}
-
-	request, err := http.Get("https://www.exploitalert.com/api/search-exploit?name=" + exploitName)
+	request, err := http.Get("https://www.exploitalert.com/api/search-exploit?name=" + *argparserName)
 	defer request.Body.Close()
 
 	if err != nil {
@@ -45,7 +40,7 @@ func main() {
 	fmt.Printf("I found the following Exploit's: \033[1;35m%d\033[0;0m\n", len(jsn))
 
 	for i := 0; i < len(jsn); i++ {
-		if i < 20 {
+		if i < *argparserPage {
 			fmt.Printf("— \033[1;35mID:\033[0;0m %s \033[1;35mDate:\033[0;0m %s \033[1;35mName:\033[0;0m %s\n", jsn[i].ID, jsn[i].Date, jsn[i].Name)
 		} else {
 			break
@@ -54,8 +49,7 @@ func main() {
 }
 
 func Banner() {
-	fmt.Print("\033[1;96m")
-	fmt.Println(`—————————————————————————————————————————————————————————————————————————————
+	fmt.Println("\033[1;96m", `—————————————————————————————————————————————————————————————————————————————
  ██████╗  ██████╗       ███████╗██╗  ██╗██████╗ ██╗      ██████╗ ██╗████████╗
 ██╔════╝ ██╔═══██╗      ██╔════╝╚██╗██╔╝██╔══██╗██║     ██╔═══██╗██║╚══██╔══╝
 ██║  ███╗██║   ██║█████╗█████╗   ╚███╔╝ ██████╔╝██║     ██║   ██║██║   ██║   
@@ -63,6 +57,5 @@ func Banner() {
 ╚██████╔╝╚██████╔╝      ███████╗██╔╝ ██╗██║     ███████╗╚██████╔╝██║   ██║   
  ╚═════╝  ╚═════╝       ╚══════╝╚═╝  ╚═╝╚═╝     ╚══════╝ ╚═════╝ ╚═╝   ╚═╝   																		 
 		By: www.github.com/blkzy — Version: 1.0
-—————————————————————————————————————————————————————————————————————————————`)
-	fmt.Print("\033[0;0m")
+—————————————————————————————————————————————————————————————————————————————`, "\033[0;0m")
 }
